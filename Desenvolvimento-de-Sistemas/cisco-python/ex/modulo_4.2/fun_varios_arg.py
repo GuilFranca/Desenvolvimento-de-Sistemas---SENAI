03dac1c9c7a7d496dbd26bc43d8fb687898b76c4
GuilFranca <guilfranca27@gmail.com>
2024-11-12 08:41:11 -0300

    att 11/11

---
+++ Desenvolvimento-de-Sistemas/cisco-python/ex/modulo_4.2/fun_varios_arg.py
@@ -0,0 +1,8 @@
+def message(name1, name2):
+    print(f"Bom dia {name1}")
+    print(f"Bom dia {name2}")
+
+name1 = input("Digite o seu nome: ")
+name2 = input("Digite o seu nome: ")
+
+message(name1, name2)
