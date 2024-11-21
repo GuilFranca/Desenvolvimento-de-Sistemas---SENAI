4a1c6a93ac0f6c1660146468fa4973ecb3838651
GuilFranca <guilfranca27@gmail.com>
2024-11-21 09:56:35 -0300

    correção exercício 2

--- Desenvolvimento-de-Sistemas/Python_New/exercícios/procedimentos/att_02.py
+++ Desenvolvimento-de-Sistemas/Python_New/exercícios/procedimentos/att_02.py
@@ -1,10 +1,18 @@
 # 2) Crie uma função fala se um número é par ou ímpar. Retorne se o número é par ou ímpar.
 
-def paridade(num):
-    if num % 2 != 0:
-        print(f"O número {num} é impar")
-    else:
-        print(f"O número {num} é par")
+# def paridade(num):
+#     if num % 2 != 0:
+#         print(f"O número {num} é impar")
+#     else:
+#         print(f"O número {num} é par")
 
-num = int(input("Digite um número: "))
-paridade(num)
+# num = int(input("Digite um número: "))
+# paridade(num)
+
+def parImpar(numero):
+    if numero % 2 == 0:
+        return 'Par'
+    else:
+        return 'Impar'
+
+print(parImpar(4))
